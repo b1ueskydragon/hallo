76a78be14eff22b820b7d420b5741585bca3c62f
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2017-10-22 15:58:26 +0900

    Main extends Application

--- src/main/Execute.java
+++ src/main/Execute.java
@@ -1,10 +1,20 @@
 package main;
 
 import finder.SearchTarget;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Execute {
+import javax.swing.*;
+
+public class Execute extends Application {
 
   public static void main(String ... halloween) {
+    new JFrame().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // TODO 不要ならら消す
+    launch(halloween);
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
     SearchTarget searchTarget = new SearchTarget();
     searchTarget.getTargetList()
         .forEach(file -> System.out.println(file.getName()));
