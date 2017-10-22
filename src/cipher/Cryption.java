d1bbfea503ea65aeff10bed8656203f24306aa98
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2017-10-23 00:18:45 +0900

    Remove initVector params

--- src/cipher/Cryption.java
+++ src/cipher/Cryption.java
@@ -7,11 +7,9 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
-import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
 import java.util.List;
 
 public class Cryption {
@@ -23,10 +21,9 @@ public class Cryption {
    *
    * @param input      ファイル
    * @param commonKey  共通キーになる秘密キー (128 bit key)
-   * @param initVector 初期化ベクトル (16 bytes IV)
    * @param output     暗号化されて排出されるファイル
    */
-  public void fileEncrypt(int cryptMode, String commonKey, String initVector, File input, File output) {
+  public void fileEncrypt(int cryptMode, String commonKey, File input, File output) {
     IvParameterSpec iv;
     Key skeySpec;
     Cipher cipher;
@@ -36,10 +33,9 @@ public class Cryption {
 
     try {
       // 暗号化ロジック
-      iv = new IvParameterSpec(initVector.getBytes());
       skeySpec = new SecretKeySpec(commonKey.getBytes(), "AES");
       cipher = Cipher.getInstance("AES");
-      cipher.init(cryptMode, skeySpec, iv);
+      cipher.init(cryptMode, skeySpec); // ここに 16byte の initVector 追加したかった
 
       // File IO
       inputStream = new FileInputStream(input);
@@ -51,7 +47,7 @@ public class Cryption {
     } catch (NoSuchAlgorithmException
         | NoSuchPaddingException | BadPaddingException
         | InvalidKeyException | IllegalBlockSizeException
-        | InvalidAlgorithmParameterException | IOException e) {
+        | IOException e) {
       e.printStackTrace();
 
     } finally {
@@ -70,7 +66,7 @@ public class Cryption {
    * @param files     複数のファイル
    */
   public void exeEncrypt(List<File> files) {
-    files.forEach(file -> fileEncrypt(Cipher.ENCRYPT_MODE, CryptionKeys.COMMON_KEY, CryptionKeys.INIT_VECTOR,
+    files.forEach(file -> fileEncrypt(Cipher.ENCRYPT_MODE, CryptionKeys.COMMON_KEY,
             file, new File(file.getPath() + ".hallo")));
     System.out.println("[log] 暗号化完了");
   }
