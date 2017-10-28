bc5110f3b74c1a016ba8a365607ea5057574e908
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2017-10-29 01:06:23 +0900

    Add javadoc

--- src/tools/MailSendProperties_sample.java
+++ src/tools/MailSendProperties_sample.java
@@ -3,11 +3,29 @@ package tools;
 /**
  * Gmail の場合、セキュリティ上、このアプリからの接近を有効に変更すべき
  */
-public class MailSendProperties_sample {
-  final static String TO = ""; // 受信者のメールアドレス
-  final static String FROM = ""; // 返信先 (半角英数推奨)
-  final static String USER_NAME = ""; // 送信者のメールアドレス
-  final static String PASSWORD = ""; // 送信者のメールアドレスのパスワード
+class MailSendProperties_sample {
+  /**
+   * 受信者のメールアドレス
+   */
+  final static String TO = "";
 
-  final static String PERSONAL = ""; // 送信者の名前 (半角英数推奨)
+  /**
+   * 返信先 (半角英数推奨)
+   */
+  final static String FROM = "";
+
+  /**
+   * 送信者のメールアドレス
+   */
+  final static String USER_NAME = "";
+
+  /**
+   * 送信者のメールアドレスのパスワード
+   */
+  final static String PASSWORD = "";
+
+  /**
+   * 送信者の名前 (半角英数推奨)
+   */
+  final static String PERSONAL = "";
 }
