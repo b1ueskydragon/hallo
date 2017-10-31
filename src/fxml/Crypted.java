c857df7d074007f4d3c6ab487db7b855d8e5b06e
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2017-10-31 09:01:55 +0900

    Make fxml version as 1

--- src/fxml/Crypted.java
+++ src/fxml/Crypted.java
@@ -3,20 +3,21 @@ package fxml;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
-import java.awt.*;
 
 public class Crypted {
   @FXML
-  private Parent parent;
+  private Parent root;
   @FXML
-  private TextField textField;
+  private TextField inputKey;
   @FXML
   private Button button;
   // TODO 本当は fx:id 名前指定したい
 
   public void handleInputAction(ActionEvent event) {
-    String key = textField.getText();
+    String key = inputKey.getText();
     System.out.println(key);
   }
 
--- src/fxml/_FxmlTestCrypted_.java
+++ src/fxml/_FxmlTestCrypted_.java
@@ -13,8 +13,8 @@ public class _FxmlTestCrypted_ extends Application {
   @Override
   public void start(Stage primaryStage) throws Exception {
 
-    Parent parent = FXMLLoader.load(getClass().getResource("Crypted.fxml"));
-    Scene scene = new Scene(parent);
+    Parent root = FXMLLoader.load(getClass().getResource("Crypted.fxml"));
+    Scene scene = new Scene(root);
 
     primaryStage.setTitle("hallo");
     primaryStage.setScene(scene);
