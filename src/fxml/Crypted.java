1a1443514e3e4db94dbc3ff11041ca5eeab6a4ae
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2017-10-31 09:14:31 +0900

    Add alerts

--- src/fxml/Crypted.java
+++ src/fxml/Crypted.java
@@ -3,8 +3,10 @@ package fxml;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import tools.StringUtility;
 
 
 public class Crypted {
@@ -14,11 +16,25 @@ public class Crypted {
   private TextField inputKey;
   @FXML
   private Button button;
-  // TODO 本当は fx:id 名前指定したい
 
-  public void handleInputAction(ActionEvent event) {
-    String key = inputKey.getText();
-    System.out.println(key);
+  String commonKey = "aaa";
+
+  public void handleInputAction() {
+    String userKey = inputKey.getText();
+    System.out.println(userKey);
+    Alert alert;
+    if (StringUtility.isNullOrEmpty(this.commonKey)) return;
+    if (this.commonKey.equals(userKey)) {
+      alert = new Alert(Alert.AlertType.INFORMATION);
+      alert.setTitle("Success");
+      alert.setHeaderText("Decryption Start");
+      alert.showAndWait();
+    } else {
+      alert = new Alert(Alert.AlertType.ERROR);
+      alert.setTitle("Failed");
+      alert.setHeaderText("だめだめ");
+      alert.showAndWait();
+    }
   }
 
 
